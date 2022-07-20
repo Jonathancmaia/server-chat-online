6756fc61b1927d3dde8110f56b89a9cf379a2047
jonathancmaia <jonathancmaia@gmail.com>
2022-07-20 20:51:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const cors = require('cors');
+const { v4 } = require('uuid');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server,{
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  }
+});
+
+const port = 3000;
+
+let messages = [];
+
+//Routes
+
+app.get('/', (req, res)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  res.send(v4());
+});
+
+//Events
+io.on('connect', (socket) => {
+
+  //Join treatment
+
+  const room = socket.handshake.query.room;
+  const user = socket.id;
+
+  socket.join(room);
+  
+  socket.on('newUserConnected', () =>{
+    socket.emit('getUser', user);
+
+    let userList= io.sockets.adapter.rooms.get(room);
+
+    let arrUserList = undefined;
+
+    if (userList !== undefined){
+      arrUserList = Array.from(userList);
+    } else {
+      arrUserList = arrUserList;
+    }
+    
+    io.in(room).emit('getUserList', Array.from(userList));
+
+    if(messages[room]){
+      io.in(room).emit('getChat', messages[room]);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    let userList = io.sockets.adapter.rooms.get(room);
+
+    let arrUserList = undefined;
+
+    if (userList !== undefined){
+      arrUserList = Array.from(userList);
+    } else {
+      arrUserList = arrUserList;
+    }
+
+    io.in(room).emit('getUserList', arrUserList);
+    io.in(room).emit('removeVideo', user);
+  });
+
+  //Messages treatment
+
+  socket.on('newMessage', (args) => {
+    if(messages[room] === undefined){
+      messages[room] = [];
+      messages[room].push({message: args.message, user: args.user});
+    } else {
+      messages[room].push({message: args.message, user: args.user});
+    }
+
+    io.in(room).emit('getChat', messages[room]);
+  });
+
+  io.of("/").adapter.on("delete-room", (room) => {
+    if(messages[room] !== undefined){
+      delete messages[room];
+    }
+  });
+});
+
+io.listen(2000);
+app.listen(port);
