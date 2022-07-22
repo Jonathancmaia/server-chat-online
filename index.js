a4496e7ec9be21a7df86b21f37fddad5bfd9f102
jonathancmaia <jonathancmaia@gmail.com>
2022-07-22 19:20:31 -0300

    try 2817628 to setup peerjs on heroku

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ io.on('connect', (socket) => {
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 var server = require('http').createServer(app);
 
-server.listen(9000);
+server.listen(process.env.PORT || 9000);
 
 var options = {
   debug: true
