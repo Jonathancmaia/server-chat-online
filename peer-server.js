3f69e71cfc80efb70b4b10912993d228ef9d4f30
jonathancmaia <jonathancmaia@gmail.com>
2022-11-12 17:17:47 -0300

    change certificate root on peer-server

--- peer-server.js
+++ peer-server.js
@@ -4,13 +4,22 @@ var ExpressPeerServer = require('peer').ExpressPeerServer;
 const https = require('https');
 const fs = require('fs');
 
-const httpsServer = https.createServer({
-    cert: fs.readFileSync('free-chat-online.cf/fullchain.pem'),
-    key: fs.readFileSync('free-chat-online.cf/privkey.pem')
-  }, app).listen(9000);
- 
+const httpsServer = https
+  .createServer(
+    {
+      cert: fs.readFileSync(
+        '/etc/letsencrypt/live/free-chat-online.cf/fullchain.pem',
+      ),
+      key: fs.readFileSync(
+        '/etc/letsencrypt/live/free-chat-online.cf/privkey.pem',
+      ),
+    },
+    app,
+  )
+  .listen(9000);
+
 var options = {
-    debug: true
-}
- 
+  debug: true,
+};
+
 app.use('/', ExpressPeerServer(httpsServer, options));
